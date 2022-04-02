660df1d3364164f047f5fd2ecea7c624f3c2c773
lars <lars.scheme@gmail.com>
2022-04-02 09:48:51 +0200

    Quit with ESC, fullscreen with F11

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import "github.com/gonutz/prototype/draw"
+
 func main() {
+	fullscreen := false
+	draw.RunWindow("Delay the Inevitable", 1000, 800, func(window draw.Window) {
+		if window.WasKeyPressed(draw.KeyEscape) {
+			window.Close()
+			return
+		}
+
+		if window.WasKeyPressed(draw.KeyF11) {
+			fullscreen = !fullscreen
+		}
+		window.SetFullscreen(fullscreen)
+
+		window.DrawText("ESC to quit\nF11 to toggle fullscreen", 0, 0, draw.White)
+	})
 }
